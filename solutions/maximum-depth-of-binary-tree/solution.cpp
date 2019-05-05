e6d1a5fa47e6d79c6bcb5ad08433c2e73f47c7eb
Vladimir Davydov <vdavydov.dev@gmail.com>
2019-05-05 16:13:45 +0300

    Maximum Depth of Binary Tree

---
+++ solutions/maximum-depth-of-binary-tree/solution.cpp
@@ -0,0 +1,104 @@
+#include <algorithm>
+#include <iostream>
+#include <queue>
+#include <vector>
+
+using namespace std;
+
+struct TreeNode {
+	int val;
+	TreeNode *left;
+	TreeNode *right;
+	TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+};
+
+ostream& operator<<(ostream& out, const TreeNode* root)
+{
+	cout << '[';
+	queue<const TreeNode*> todo;
+	todo.push(root);
+	int null_count = 0;
+	while (!todo.empty()) {
+		auto node = todo.front();
+		todo.pop();
+		if (node) {
+			todo.push(node->left);
+			todo.push(node->right);
+			if (node != root)
+				cout << ',';
+			while (null_count > 0) {
+				cout << "null,";
+				--null_count;
+			}
+			cout << node->val;
+		} else {
+			++null_count;
+		}
+	}
+	cout << ']';
+	return out;
+}
+
+// Assume 0 means no value.
+TreeNode* makeTree(const vector<int>& values)
+{
+	if (values.empty())
+		return nullptr;
+	auto it = values.begin();
+	auto root = new TreeNode(*it++);
+	queue<TreeNode*> todo;
+	todo.push(root);
+	while (it != values.end()) {
+		auto node = todo.front();
+		todo.pop();
+		int val;
+		if ((val = *it++) != 0) {
+			node->left = new TreeNode(val);
+			todo.push(node->left);
+		}
+		if (it != values.end() && (val = *it++) != 0) {
+			node->right = new TreeNode(val);
+			todo.push(node->right);
+		}
+	}
+	return root;
+}
+
+void deleteTree(TreeNode* root)
+{
+	if (root) {
+		deleteTree(root->left);
+		deleteTree(root->right);
+		delete root;
+	}
+}
+
+class Solution {
+public:
+	int maxDepth(const TreeNode* root) {
+		if (!root)
+			return 0;
+		return 1 + max(maxDepth(root->left), maxDepth(root->right));
+	}
+};
+
+int main()
+{
+	vector<int> input[] = {
+		{}, // 0
+		{1}, // 1
+		{1, 2}, // 2
+		{1, 0, 2}, // 2
+		{1, 2, 3, 0, 4}, // 3
+		{1, 2, 3, 0, 4, 0, 5, 6}, // 4
+		{1, 2, 0, 3, 0, 4, 0, 5, 0}, // 5
+	};
+	Solution solution;
+	for (const auto& values: input) {
+		auto root = makeTree(values);
+		cout << "Max depth of " << root << " is " <<
+			solution.maxDepth(root) << endl;
+		deleteTree(root);
+	}
+	return 0;
+}
